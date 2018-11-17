9332cadef65d3f824a7b2f1689bd897f11a4c0f9
zhangchen <18310024875@163.com>
2018-11-17 14:11:52 +0800

    ok

--- 阿里云.js
+++ 阿里云.js
@@ -2,37 +2,57 @@
 163邮箱
 18310024875 qwerqwer
 
+
 github 
 18310024875@163.com 
 a13070198152
 
 
-阿里云 
-张琛1992 a13070198152   ; 18310024875 qwerqwer
+阿里云账号-->
+	张琛1992 a13070198152   ; 18310024875 qwerqwer
 
 
+服务器信息-->
+	公网IP: 39.105.201.170 ;
+	内网IP: 172.17.148.162 ;
+	登录名 root ;
+	登录密码 Qq123456 ;
 
-实例名称: iZ2ze7qiq326ve73m86kqvZ ;
-公网IP: 39.105.201.170 ;
-内网IP: 172.17.148.162 ;
-登录名 root ;
-登录密码 Aa123456 ;
-若您忘记或未设置密码可进入ECS控制台-实例列表-重置密码。
 
-阿里云控制台--> 远程连接密码: 140190 ;
+阿里云控制台远程连接-->
+	密码: 140190 ;
+
 
 ssh连接-->远程连接
+	( rm -rf ~/.ssh/known_hosts )
 	sudo -i,
-	ssh 39.105.201.170
-	Aa123456
+	ssh 39.105.201.170 
+		
+		Qq123456
 
-yum 教程 https://www.cnblogs.com/liaocheng/p/4243589.html
-
-安装 VSFTPD //https://www.linuxidc.com/Linux/2017-11/148518.htm
 
 
 -------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-处理进程 https://www.cnblogs.com/aipiaoborensheng/p/7676364.html
+
+数据库MariaDB --->
+	( yum -y install mariadb mariadb-server ---> SET PASSWORD FOR 'root'@'localhost' = PASSWORD('newpass');  )
+	root qwer 
+	systemctl start mariadb
+	systemctl restart mariadb
+	systemctl stop mariadb
+	SHOW VARIABLES LIKE 'character%'
+
+
+教程 --->
+	node https://blog.csdn.net/xerysherryx/article/details/78920978
+
+	yum https://www.cnblogs.com/liaocheng/p/4243589.html
+
+	VSFTPD //https://www.linuxidc.com/Linux/2017-11/148518.htm
+
+
+处理进程 --->
+	https://www.cnblogs.com/aipiaoborensheng/p/7676364.html
 	netstat -lntp  #查看监听(Listen)的端口
 	netstat -tulpn #查看所有运行中的服务的详细信息
 	ps -aux        #显示使用内存的进程
@@ -42,6 +62,12 @@ yum 教程 https://www.cnblogs.com/liaocheng/p/4243589.html
     ps -A 显示所有程序。
     ps e 列出程序时，显示每个程序所使用的环境变量。
     kill －9 324
+
+-------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+页面地址
+	http://39.105.201.170:3000/index.html
 `
 
-http://39.105.201.170:3000/index.html
+
+
